3151a8b5a3441496ebcc1353728a2191ec9b0d70
TorlapSayHi <torlap.ritchai@gmail.com>
2025-12-23 14:42:01 +0700

    test(superadmin): refactor community deletion tests for clarity and functionality

--- tests/superadmin/TS-DCMNT-02.spec.js
+++ tests/superadmin/TS-DCMNT-02.spec.js
@@ -2,6 +2,7 @@ import { test, expect } from "@playwright/test";
 import { loginAs } from "../../utils/roles.js";
 import path from "path";
 
+const nameTestFile = "วิสาหกิจชุมชนกลุ่มผลิตผ้าย้อมครามบ้านคำข่า";
 test.describe("SuperAdmin - Manage Community", () => {
     test.beforeEach(async ({ page }) => {
         await loginAs(page, "superadmin");
@@ -12,31 +13,42 @@ test.describe("SuperAdmin - Manage Community", () => {
     * TS-DCMNT-02.1: คลิกปุ่มไอคอน (Icon Button) ถังขยะ ต้องโชว์หน้าต่างแสดงผลซ้อน (Modal)
     */
     test("TS-DCMNT-02.1: คลิกปุ่มไอคอน (Icon Button) ถังขยะ ต้องโชว์หน้าต่างแสดงผลซ้อน (Modal)", async ({ page }) => {
-        await filterAllCommunityManagePage(page);
+        await openModalDeleteCommunity(page);
     });
 
     /*
     * TS-DCMNT-02.2: คลิกปุ่ม (Button) ยืนยัน ในหน้าต่างแสดงผลซ้อน (Modal) ชุมชนจะถูกลบ
     */
     test("TS-DCMNT-02.2: คลิกปุ่ม (Button) ยืนยัน ในหน้าต่างแสดงผลซ้อน (Modal) ชุมชนจะถูกลบ", async ({ page }) => {
-        await filterOpenCommunityManagePage(page);
+        await confirmDeleteCommunity(page);
     });
 
     /*
     * TS-DCMNT-02.3: คลิกปุ่ม (Button) ยกเลิก ในหน้าต่างแสดงผลซ้อน (Modal) ชุมชนจะไม่ถูกลบ
     */
     test("TS-DCMNT-02.3: คลิกปุ่ม (Button) ยกเลิก ในหน้าต่างแสดงผลซ้อน (Modal) ชุมชนจะไม่ถูกลบ", async ({ page }) => {
-        await filterCloseCommunityManagePage(page);
+        await cancelDeleteCommunity(page);
     });
 
 
-    async function filterAllCommunityManagePage(page) {
+    async function openModalDeleteCommunity(page) {
+        await page.getByRole('link', { name: 'จัดการชุมชน' }).click();
+        await expect(page).toHaveURL(/super\/communities\/all/);
+        await page.getByRole('row', { name: nameTestFile }).getByLabel('ลบ').click();
     }
 
-    async function filterOpenCommunityManagePage(page) {
+    async function confirmDeleteCommunity(page) {
+        await page.getByRole('link', { name: 'จัดการชุมชน' }).click();
+        await expect(page).toHaveURL(/super\/communities\/all/);
+        await page.getByRole('row', { name: nameTestFile }).getByLabel('ลบ').click();
+        await page.getByRole('button', { name: 'ยืนยัน' }).click();
     }
 
-    async function filterCloseCommunityManagePage(page) {
+    async function cancelDeleteCommunity(page) {
+        await page.getByRole('link', { name: 'จัดการชุมชน' }).click();
+        await expect(page).toHaveURL(/super\/communities\/all/);
+        await page.getByRole('row', { name: nameTestFile }).getByLabel('ลบ').click();
+        await page.getByRole('button', { name: 'ยกเลิก' }).click();
     }
 
 });
